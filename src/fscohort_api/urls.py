fcd029040560679cf4ae8bb2e2fab0f9c819f0c5
omeriliski <omeriliski06@gmail.com>
2021-01-14 00:23:23 +0100

    added class base

--- src/fscohort_api/urls.py
+++ src/fscohort_api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import student_list,student_create,student_list_create,student_get_update_delete
+from .views import student_list,student_create,student_list_create,student_get_update_delete,StudentListClass,StudentGetUpdateDeleteClass
 
 urlpatterns=[
-    path("student_list/",student_list),
-    path("student_create/",student_create),
-    path("student_list_create/",student_list_create),
-    path("<int:id>",student_get_update_delete)
+    path("list/",student_list),
+    path("create/",student_create),
+    path("list_create/",student_list_create),
+    # path("<int:id>/",student_get_update_delete),
+    path("list_class/",StudentListClass.as_view()),
+    path("<int:id>/",StudentGetUpdateDeleteClass.as_view()),
 ]
--- src/fscohort_api/views.py
+++ src/fscohort_api/views.py
@@ -2,6 +2,7 @@ from datetime import date
 from django.shortcuts import get_object_or_404, render
 from django.http import JsonResponse
 from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
 from fscohort.models import Student
 from django.core.serializers import serialize
 import json
@@ -12,6 +13,7 @@ from rest_framework.decorators import api_view
 from .serializers import StudentSerializer
 from rest_framework import status
 
+# Function Base
 def student_list(request):
     if request.method=="GET":
         students = Student.objects.all()
@@ -81,3 +83,46 @@ def student_get_update_delete(request,id):
     if request.method == "DELETE":
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Class Base
+class StudentListClass(APIView):
+    def get(self,request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students,many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response( serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class StudentGetUpdateDeleteClass(APIView):
+    
+    def get_object(self,id):
+        try:
+            return Student.objects.get(id=id)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def get(self,request,id):
+        student = self.get_object(id)
+        # student = get_object_or_404(Student,id=id) böyle de olur,yukarıyı yazmadan
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    def put(self,request,id):
+        student = self.get_object(id)
+        serializer = StudentSerializer(student,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response( serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,id):
+        student = self.get_object(id)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
